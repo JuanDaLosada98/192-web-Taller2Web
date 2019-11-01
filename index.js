bb6d9730575cca519b5315a9f4d3f1af59042e96
Juan David Losada Urriago <1107516477@academico.icesi.edu.co>
2019-11-01 07:32:41 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const exphbs  = require('express-handlebars');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+
+// Set handlebars middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+// Set handlebars routes
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+// Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(PORT, () => console.log('El servidor esta escuchando en el puerto: '+PORT));
