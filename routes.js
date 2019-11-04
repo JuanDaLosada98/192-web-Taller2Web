b8b5076d2cfff11c43144912b0911362c1599dfc
JuanDaLosada98 <juandavidlosada2@gmail.com>
2019-11-03 22:32:18 -0500

    ya filtra y ordena

--- routes.js
+++ routes.js
@@ -23,10 +23,168 @@ function createRoutes (app, db) {
             .toArray((err, result) => {
                 //aseguramos de que no hay error
                 assert.equal(null, err);
+                var listCopy = result.slice();
+
+                if (request.query.filter == "0-$10.000") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.price >= 0 && elem.price <= 10000) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "$10.000-$20.000") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.price >= 10000 && elem.price <= 20000) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "$20.000-$30.000") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.price >= 20000 && elem.price <= 30000) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "$30.000-$40.000") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.price >= 30000 && elem.price <= 40000) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "$40.000-$50.000") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.price >= 40000 && elem.price <= 50000) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "$50.000-$100.000") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.price >= 50000 && elem.price <= 100000) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "big") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.size === 3) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "medium") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.size === 2) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+                
+                if (request.query.filter == "small") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.size === 1) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "plasma") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.danger === 1) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "electric") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.danger === 2) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == "plumb") {
+                    listCopy = listCopy.filter(function(elem) {
+                        if (elem.danger === 3) {
+                            return true;
+                        } else {
+                            return false;
+                        }
+                    });
+                }
+
+                if (request.query.filter == 'fromoreless') {
+                    listCopy.sort(function(a, b) {
+                        return b.popularity - a.popularity;
+                    });
+                }
+
+                if (request.query.filter == 'fromlessmore') {
+                    listCopy.sort(function(a, b) {
+                        return a.popularity - b.popularity;
+                    });
+                }
+                
+                if (request.query.filter == 'lh') {
+                    listCopy.sort(function(a, b) {
+                        return a.price - b.price;
+                    });
+                }
+
+                if (request.query.filter == 'high to low') {
+                    listCopy.sort(function(a, b) {
+                        return b.price - a.price;
+                    });
+                }
+
+                if (request.query.filter == 'from more to less') {
+                    listCopy.sort(function(a, b) {
+                        return b.rareness - a.rareness;
+                    });
+                }
+
+                if (request.query.filter == 'from less to more') {
+                    listCopy.sort(function(a, b) {
+                        return a.rareness - b.rareness;
+                    });
+                }
 
                 var context = {
-                    products: result
+                    products: listCopy
                 };
+
 
                 response.render('store',context);
         });
