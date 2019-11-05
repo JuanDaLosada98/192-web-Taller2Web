acaa3e6bc0aa3c551fe8a6027f6229b2de0639c7
JuanDaLosada98 <juandavidlosada2@gmail.com>
2019-11-05 17:52:36 -0500

    vista del producto totalmente maquetada y carrito desplegable en proceso

---
+++ public/scripts/cart.js
@@ -0,0 +1,10 @@
+var btn = document.querySelector('.carshop');
+var list = document.querySelector('.listCartR');
+
+function handleClick(){
+    list.classList.toggle('listCartR--active');
+    btn.classList.toggle('carshop--active');
+   
+}
+
+btn.addEventListener('click', handleClick);
